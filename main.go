d4f73afcd35e7046b75d78337a6f8404d11782ab
Sophia <beyondthesophia@gmail.com>
2021-10-12 16:32:17 +0900

    update main.go

--- main.go
+++ main.go
@@ -1,10 +1,15 @@
 package main
 
 import (
-	"identifier/ygopro-deck-identifier"
+	ygopro_data "github.com/iamipanda/ygopro-data"
+	ygopro_deck_identifier "identifier/ygopro-deck-identifier"
+	"os"
+	"path/filepath"
 )
 
 func main() {
+	ygopro_data.LuaPath = filepath.Join(os.Getenv("GOPATH"), "pkg/mod/github.com/iamipanda/ygopro-data@v0.0.0-20190116110429-360968dc5c66/Constant.lua")
+
 	ygopro_deck_identifier.Initialize()
 	ygopro_deck_identifier.StartServer()
 }

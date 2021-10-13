c8bb00c3b3192e17fc58fb15264fa7c59441e7ed
Sophia <beyondthesophia@gmail.com>
2021-10-13 16:47:35 +0900

    make identifier automatically fetch latest version of resources

--- go.mod
+++ go.mod
@@ -3,9 +3,19 @@ module identifier
 go 1.16
 
 require (
+	github.com/ProtonMail/go-crypto v0.0.0-20210920160938-87db9fbc61c7 // indirect
 	github.com/gin-gonic/gin v1.7.4 // indirect
+	github.com/go-git/go-git/v5 v5.4.2 // indirect
 	github.com/iamipanda/ygopro-data v0.0.0-20190116110429-360968dc5c66 // indirect
 	github.com/itchio/lzma v0.0.0-20190703113020-d3e24e3e3d49 // indirect
+	github.com/kevinburke/ssh_config v1.1.0 // indirect
 	github.com/mattn/go-sqlite3 v1.14.8 // indirect
 	github.com/op/go-logging v0.0.0-20160315200505-970db520ece7 // indirect
+	github.com/sergi/go-diff v1.2.0 // indirect
+	github.com/xanzy/ssh-agent v0.3.1 // indirect
+	golang.org/x/crypto v0.0.0-20210921155107-089bfa567519 // indirect
+	golang.org/x/net v0.0.0-20211011170408-caeb26a5c8c0 // indirect
+	golang.org/x/sys v0.0.0-20211007075335-d3039528d8ac // indirect
+	gopkg.in/src-d/go-git.v4 v4.13.1 // indirect
+	gopkg.in/yaml.v2 v2.4.0 // indirect
 )
--- main.go
+++ main.go
@@ -1,14 +1,152 @@
 package main
 
 import (
-	ygopro_data "github.com/iamipanda/ygopro-data"
+	"github.com/go-git/go-billy/v5/memfs"
+	"github.com/go-git/go-git/v5"
+	"github.com/go-git/go-git/v5/plumbing/object"
+	"github.com/go-git/go-git/v5/storage/memory"
 	ygopro_deck_identifier "identifier/ygopro-deck-identifier"
+	"io/ioutil"
 	"os"
 	"path/filepath"
+	"strings"
 )
 
+func ensureDirectory(path string) (string, error) {
+	if _, err := os.Stat(path); os.IsNotExist(err) {
+		err := os.Mkdir(path, 0644)
+		return "", err
+	}
+
+	return path, nil
+}
+
+func getSavedCommitId(saveDir string) (string, error) {
+	lastCommitIdPath := filepath.Join(saveDir, ".last-commit")
+	if _, err := os.Stat(lastCommitIdPath); os.IsNotExist(err) {
+		return "", err
+	}
+
+	buffer, err := ioutil.ReadFile(lastCommitIdPath)
+	if err != nil {
+		return "", err
+	}
+
+	return string(buffer), nil
+}
+
+func getLastCommitIdOfRepository(owner string, repo string) string {
+	repoDir := filepath.Join(os.TempDir(), owner+"-"+repo)
+	err := os.RemoveAll(repoDir)
+	if err != nil {
+		return ""
+	}
+
+	r, err := git.PlainClone(repoDir, false, &git.CloneOptions{
+		URL:   "https://github.com/" + owner + "/" + repo,
+		Depth: 1,
+	})
+	if err != nil {
+		panic(r)
+	}
+
+	ref, err := r.Head()
+	if err != nil {
+		panic(r)
+	}
+
+	return ref.Hash().String()
+}
+
+func checkIfUpdatable(owner string, repo string, saveDir string) bool {
+	savedLastCommitId, err := getSavedCommitId(saveDir)
+	if err != nil {
+		return true
+	}
+
+	remoteLastCommitId := getLastCommitIdOfRepository(owner, repo)
+	if savedLastCommitId != remoteLastCommitId {
+		return true
+	}
+
+	return false
+}
+
+func doUpdate(owner string, repo string, saveDir string, fileFilter func(path string) bool) {
+	fs := memfs.New()
+	r, err := git.Clone(memory.NewStorage(), fs, &git.CloneOptions{
+		URL:      "https://github.com/" + owner + "/" + repo,
+		Depth:    1,
+		Progress: os.Stdout,
+	})
+	if err != nil {
+		panic(err)
+	}
+
+	ref, err := r.Head()
+	if err != nil {
+		panic(err)
+	}
+
+	commit, err := r.CommitObject(ref.Hash())
+	if err != nil {
+		panic(err)
+	}
+
+	tree, err := commit.Tree()
+	if err != nil {
+		panic(err)
+	}
+
+	err = tree.Files().ForEach(func(f *object.File) error {
+		if fileFilter(f.Name) {
+			file, err := fs.Open(f.Name)
+			if err != nil {
+				return err
+			}
+
+			buffer := make([]byte, f.Size)
+			_, err = file.Read(buffer)
+			if err != nil {
+				return err
+			}
+
+			savePath := filepath.Join(saveDir, filepath.Base(f.Name))
+			_, err = ensureDirectory(saveDir)
+			if err != nil {
+				return err
+			}
+
+			err = ioutil.WriteFile(savePath, buffer, 0644)
+			if err != nil {
+				return err
+			}
+		}
+
+		return nil
+	})
+	if err != nil {
+		panic(err)
+	}
+
+	err = ioutil.WriteFile(filepath.Join(saveDir, ".last-commit"), []byte(ref.Hash().String()), 0644)
+	if err != nil {
+		panic(err)
+	}
+}
+
 func main() {
-	ygopro_data.LuaPath = filepath.Join(os.Getenv("GOPATH"), "pkg/mod/github.com/iamipanda/ygopro-data@v0.0.0-20190116110429-360968dc5c66/Constant.lua")
+	if checkIfUpdatable("mycard", "ygopro-database", "./zh-CN") {
+		doUpdate("mycard", "ygopro-database", "./zh-CN", func(path string) bool {
+			return strings.HasPrefix(path, "locales/zh-CN/")
+		})
+	}
+
+	if checkIfUpdatable("mycard", "ygoruby-analytics-deck-definitions", "./ygopro-deck-identifier/Definitions/production") {
+		doUpdate("mycard", "ygoruby-analytics-deck-definitions", "./ygopro-deck-identifier/Definitions/production", func(path string) bool {
+			return true
+		})
+	}
 
 	ygopro_deck_identifier.Initialize()
 	ygopro_deck_identifier.StartServer()

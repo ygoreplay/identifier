fdc53c0d4455cfd86b1e57f9abf52f48227f4c07
Sophia <beyondthesophia@gmail.com>
2021-11-01 20:11:19 +0900

    순서 변경

--- main.go
+++ main.go
@@ -194,6 +194,9 @@ func doUpdateEnvironment() {
 func main() {
 	ygopro_data.LuaPath = filepath.Join(os.Getenv("GOPATH"), "pkg/mod/github.com/iamipanda/ygopro-data@v0.0.0-20190116110429-360968dc5c66/Constant.lua")
 
+	ygopro_deck_identifier.Initialize()
+	ygopro_deck_identifier.StartServer()
+
 	c := cron.New()
 	_ = c.AddFunc("* 1 * * * *", func() {
 		doUpdateEnvironment()
@@ -201,6 +204,4 @@ func main() {
 
 	doUpdateEnvironment()
 
-	ygopro_deck_identifier.Initialize()
-	ygopro_deck_identifier.StartServer()
 }
